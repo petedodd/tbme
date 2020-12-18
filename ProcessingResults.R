cebcce079d8e196a1ad874ca30a761f1d938acd7
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2020-12-18 14:23:42 +0000

    sketching outputs for tables

--- ProcessingResults.R
+++ ProcessingResults.R
@@ -7,44 +7,81 @@ source(here('utilities.R'))
 load(file=here('outdata/AN.Rdata'))
 
 
+## === for tables
+## --- global
+G <- AN[,.(TBM.hn.treated=sum(TBM.hn.treated),
+           TBM.hp.treated=sum(TBM.hp.treated),
+           TBM.hn.untreated=sum(TBM.hn.untreated),
+           TBM.hp.untreated=sum(TBM.hp.untreated),
+           TBM.total = sum(TBM.hn.treated) + sum(TBM.hp.treated) + sum(TBM.hn.untreated) + sum(TBM.hp.untreated),
+           TBMdeaths.hn.treated=sum(TBMdeaths.hn.treated),
+           TBMdeaths.hp.treated=sum(TBMdeaths.hp.treated),
+           TBMdeaths.hn.untreated=sum(TBMdeaths.hn.untreated),
+           TBMdeaths.hp.untreated=sum(TBMdeaths.hp.untreated),
+           TBMdeaths.total = sum(TBMdeaths.hn.treated) + sum(TBMdeaths.hp.treated) + sum(TBMdeaths.hn.untreated) + sum(TBMdeaths.hp.untreated))]
 
-## global
-G <- AN[,.(TBM.treated=sum(TBM.treated),TBM.untreated=sum(TBM.untreated),
-           TBMdeaths.treated=sum(TBMdeaths.treated),
-           TBMdeaths.untreated=sum(TBMdeaths.untreated))]
-GA <- AN[,.(TBM.treated=sum(TBM.treated),TBM.untreated=sum(TBM.untreated),
-            TBMdeaths.treated=sum(TBMdeaths.treated),
-            TBMdeaths.untreated=sum(TBMdeaths.untreated)),by=.(sex,age)]
-GA
 
-## regional
-R <- AN[,.(TBM.treated=sum(TBM.treated),TBM.untreated=sum(TBM.untreated),
-           TBMdeaths.treated=sum(TBMdeaths.treated),
-           TBMdeaths.untreated=sum(TBMdeaths.untreated)),by=g_whoregion]
-RA <- AN[,.(TBM.treated=sum(TBM.treated),TBM.untreated=sum(TBM.untreated),
-            TBMdeaths.treated=sum(TBMdeaths.treated),
-            TBMdeaths.untreated=sum(TBMdeaths.untreated)),
-         by=.(g_whoregion,sex,age)]
+
+## --- regional
+R <- AN[,.(TBM.hn.treated=sum(TBM.hn.treated),
+           TBM.hp.treated=sum(TBM.hp.treated),
+           TBM.hn.untreated=sum(TBM.hn.untreated),
+           TBM.hp.untreated=sum(TBM.hp.untreated),
+           TBM.total = sum(TBM.hn.treated) + sum(TBM.hp.treated) + sum(TBM.hn.untreated) + sum(TBM.hp.untreated),
+           TBMdeaths.hn.treated=sum(TBMdeaths.hn.treated),
+           TBMdeaths.hp.treated=sum(TBMdeaths.hp.treated),
+           TBMdeaths.hn.untreated=sum(TBMdeaths.hn.untreated),
+           TBMdeaths.hp.untreated=sum(TBMdeaths.hp.untreated),
+           TBMdeaths.total = sum(TBMdeaths.hn.treated) + sum(TBMdeaths.hp.treated) + sum(TBMdeaths.hn.untreated) + sum(TBMdeaths.hp.untreated)),
+        by=g_whoregion]
 R
+
+## TODO uncertainty calculations
+## ie analog of above for sd's: replace sum with ssum and similarly .+...+. with sqrt( .^2 + ...+.^2)
 
 G[,g_whoregion:='Global']
 GR <- rbind(R[order(g_whoregion)],G)
+GRO <- cbind(Region=GR$g_whoregion,GR[,lapply(.SD,seer),.SDcols=2:ncol(GR)])
+
+## TODO use sd's to generate UIs
+
+## --- save/write
+fwrite(GRO,file=here('results/GRO.csv'))
+save(G,file=here('outdata/G.Rdata'))
+save(R,file=here('outdata/R.Rdata'))
 
 
-GRO <- cbind(Region=GR$g_whoregion,GR[,lapply(.SD,see),.SDcols=2:5])
+## === for graphs
+
+## global
+GA <- AN[,.(TBM.hn.treated=sum(TBM.hn.treated),
+           TBM.hp.treated=sum(TBM.hp.treated),
+           TBM.hn.untreated=sum(TBM.hn.untreated),
+           TBM.hp.untreated=sum(TBM.hp.untreated),
+           TBMdeaths.hn.treated=sum(TBMdeaths.hn.treated),
+           TBMdeaths.hp.treated=sum(TBMdeaths.hp.treated),
+           TBMdeaths.hn.untreated=sum(TBMdeaths.hn.untreated),
+           TBMdeaths.hp.untreated=sum(TBMdeaths.hp.untreated)),by=.(sex,age)]
+
+## regional
+RA <- AN[,.(TBM.hn.treated=sum(TBM.hn.treated),
+            TBM.hp.treated=sum(TBM.hp.treated),
+            TBM.hn.untreated=sum(TBM.hn.untreated),
+            TBM.hp.untreated=sum(TBM.hp.untreated),
+            TBMdeaths.hn.treated=sum(TBMdeaths.hn.treated),
+            TBMdeaths.hp.treated=sum(TBMdeaths.hp.treated),
+            TBMdeaths.hn.untreated=sum(TBMdeaths.hn.untreated),
+            TBMdeaths.hp.untreated=sum(TBMdeaths.hp.untreated)),
+         by=.(g_whoregion,sex,age)]
 
 ## save out
-fwrite(GRO,file=here('results/GRO.csv'))
-
-save(G,file=here('outdata/G.Rdata'))
-save(GA,file=here('outdata/GA.Rdata'))
-save(R,file=here('outdata/R.Rdata'))
 save(RA,file=here('outdata/RA.Rdata'))
-
+save(GA,file=here('outdata/GA.Rdata'))
 ## check aggrergate implied CFR
-(1e2*G[,TBMdeaths.treated/TBM.treated]) #18.5%, cf 0.16 (0.10, 0.24) in Anna
+(1e2*G[,TBMdeaths.hn.treated/TBM.hn.treated]) # 18.5%, cf 0.16 (0.10, 0.24) in Anna
+(1e2*G[,TBMdeaths.hp.treated/TBM.hp.treated]) # 52% - bit low?
 
 
 ## TODO list
-## 1. Pete  - sketch table structure & unc + format approach
+## 1. Pete  - sketch table structure & unc + format approach DONE
 ## 2. Anna - include uncertainty and formatting
--- utilities.R
+++ utilities.R
@@ -4,4 +4,5 @@ library(data.table)
 ## utilities
 xfun <- function(X,Y,X.sd,Y.sd) X*Y*sqrt((X.sd/X)^2+(Y.sd/Y)^2) #unc for X x Y
 see <- function(x,ns=3)formatC(signif(x,ns),big.mark = ",",format='fg') #for reading big numbers
+seer <- function(x,ns=3) see(round(x),ns=ns)
 ssum <- function(x) sqrt(sum(x^2))
