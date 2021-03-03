30330a58b4ddddc8a13917f60f8d2f8fa097c83f
petedodd <pjd@pete-pop.home>
2021-03-03 17:26:02 +0000

    new table

--- ProcessingResults.R
+++ ProcessingResults.R
@@ -53,6 +53,61 @@ GRO <- cbind(Region=GR$g_whoregion,
 
 ## TODO use sd's to generate UIs
 
+
+
+## version like (I,D) x (M,F,T) x (H+,H-,T)
+## calculate
+G2 <- AN[,
+         .(
+             TBM.hp = sum(TBM.hp.treated) + sum(TBM.hp.untreated),
+             TBM.hn = sum(TBM.hn.treated) + sum(TBM.hn.untreated),
+             TBM.total = sum(TBM.hn.treated) + sum(TBM.hp.treated) +
+                 sum(TBM.hn.untreated) + sum(TBM.hp.untreated),
+             TBMdeaths.hp = sum(TBMdeaths.hp.treated) +
+                 sum(TBMdeaths.hp.untreated),
+             TBMdeaths.hn = sum(TBMdeaths.hn.treated) +
+                 sum(TBMdeaths.hn.untreated),
+             TBMdeaths.total = sum(TBMdeaths.hn.treated) +
+               sum(TBMdeaths.hp.treated) + sum(TBMdeaths.hn.untreated) +
+               sum(TBMdeaths.hp.untreated)
+         ),
+         by=.(age,sex)]
+G2
+
+## reshape
+G2 <- melt(G2,id=c('age','sex'))
+G2[,qty:=ifelse(grepl('deaths',variable),'deaths','incidence')]
+G2[,hiv:=ifelse(grepl('total',variable),'total','HIV-negative')]
+G2[grepl('hp',variable),hiv:='HIV-positive']
+G2[,c('variable'):=NULL] #ditch
+
+tmp2 <- G2[,.(value=sum(value)),by=.(qty,sex,hiv)]
+tmp2[,age:='total'] #one margin
+
+tmp <- G2[,.(value=sum(value)),by=.(age,qty,hiv)]
+tmp[,sex:='total'] #one margin
+
+tmp3 <- tmp[,.(value=sum(value)),by=.(qty,hiv)]
+tmp3[,c('sex','age'):='total'] #'corner'
+
+## tmp2 <- G2[,.(value=sum(value)),by=.(qty,hiv)]
+## tmp2[,age:='total'] #last corner
+
+G2 <- rbind(G2,tmp) #join
+G2 <- rbind(G2,tmp2)
+G2 <- rbind(G2,tmp3)
+
+G2 <- dcast(G2,qty + sex + hiv ~ age,value.var = 'value')
+G2$qty <- factor(G2$qty,levels=c('incidence','deaths'),ordered = TRUE)
+G2$sex <- factor(G2$sex,levels=c('F','M','total'),ordered = TRUE)
+G2$hiv <- factor(G2$hiv,levels=c('HIV-negative','HIV-positive','total'),
+                 ordered = TRUE)
+G2 <- G2[order(qty,sex,hiv)]
+
+## format
+GO2 <- cbind(G2[,.(qty,sex,hiv)],
+             G2[,lapply(.SD,seer),.SDcols=4:ncol(G2)])
+
 ## --- save/write
 fwrite(GRO,file=here('results/GRO.csv'))
 GROt <- transpose(GRO)
@@ -61,6 +116,7 @@ fwrite(GROt,file=here('results/GROt.csv'),col.names = FALSE)
 save(G,file=here('outdata/G.Rdata'))
 save(R,file=here('outdata/R.Rdata'))
 
+fwrite(GO2,file=here('results/GO2.csv'))
 
 ## === for graphs
 
@@ -97,3 +153,4 @@ save(GA,file=here('outdata/GA.Rdata'))
 ## TODO list
 ## 1. Pete  - sketch table structure & unc + format approach DONE
 ## 2. Anna - include uncertainty and formatting
+## table that is:
